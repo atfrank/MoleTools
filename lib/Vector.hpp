148f90b55968bb7f33debbd69f18291dea5835a6
Sean Law <seanlaw@umich.edu>
2013-01-29 12:47:57 -0500

    Added several operators to Vector class

--- lib/Vector.hpp
+++ lib/Vector.hpp
@@ -7,27 +7,176 @@ class Vector {
     double z;
 
   public:
-    Vector(double xin=0.0, double yin=0.0, double zin=0.0) :
-      x(xin), y(yin), z(zin) {} //constructor initialization
+    Vector();
+    Vector(double xin, double yin, double zin); //Constructor
+    Vector(const Vector& vec); //Overload constructor 
 
-    Vector(const Vector& vec){
-      x=vec.x;
-      y=vec.y;
-      z=vec.z;
-    }
-
-    Vector& operator=(const Vector& vec){
-      x=vec.x;
-      y=vec.y;
-      z=vec.z;
-      return(*this);
-    }
-
-    Vector& operator=(const double val){
-      x=val;
-      y=val;
-      z=val;
-      return(*this);
-    }
-
+    Vector& operator= (const Vector& vec);
+    Vector& operator= (const double val);
+    Vector& operator+ (const Vector& vec);
+    Vector& operator+= (const Vector& vec);
+    Vector& operator+ (const double val);
+    Vector& operator+= (const double val);
+    Vector& operator- (const Vector& vec);
+    Vector& operator-= (const Vector& vec);
+    Vector& operator- (const double val);
+    Vector& operator-= (const double val);
+    Vector& operator* (const Vector& vec);
+    Vector& operator*= (const Vector& vec);
+    Vector& operator* (const double val);
+    Vector& operator*= (const double val);
+    Vector& operator/ (const Vector& vec);
+    Vector& operator/= (const Vector& vec);
+    Vector& operator/ (const double val);
+    Vector& operator/= (const double val);
 };
+
+Vector::Vector(){
+  x=0.0;
+  y=0.0;
+  z=0.0;
+}
+
+Vector::Vector(double xin, double yin, double zin){
+  x=xin;
+  y=yin;
+  z=zin;
+}
+
+Vector::Vector(const Vector& vec){
+      x=vec.x;
+      y=vec.y;
+      z=vec.z;
+    }
+
+Vector& Vector::operator= (const Vector& vec){
+  x=vec.x;
+  y=vec.y;
+  z=vec.z;
+  return(*this);
+}
+
+Vector& Vector::operator= (const double val){
+  x=val;
+  y=val;
+  z=val;
+  return(*this);
+}
+
+//Addition
+Vector& Vector::operator+ (const Vector& vec){
+  x+=vec.x;
+  y+=vec.y;
+  z+=vec.z;
+  return(*this);
+}
+
+Vector& Vector::operator+= (const Vector& vec){
+  x+=vec.x;
+  y+=vec.y;
+  z+=vec.z;
+  return(*this);
+}
+
+Vector& Vector::operator+ (const double val){
+  x+=val;
+  y+=val;
+  z+=val;
+  return(*this);
+}
+
+Vector& Vector::operator+= (const double val){
+  x+=val;
+  y+=val;
+  z+=val;
+  return(*this);
+}
+
+//Subtraction
+Vector& Vector::operator- (const Vector& vec){
+  x-=vec.x;
+  y-=vec.y;
+  z-=vec.z;
+  return(*this);
+}
+
+Vector& Vector::operator-= (const Vector& vec){
+  x-=vec.x;
+  y-=vec.y;
+  z-=vec.z;
+  return(*this);
+}
+
+Vector& Vector::operator- (const double val){
+  x-=val;
+  y-=val;
+  z-=val;
+  return(*this);
+}
+
+Vector& Vector::operator-= (const double val){
+  x-=val;
+  y-=val;
+  z-=val;
+  return(*this);
+}
+
+//Multiplication
+Vector& Vector::operator* (const Vector& vec){
+  x*=vec.x;
+  y*=vec.y;
+  z*=vec.z;
+  return(*this);
+}
+
+Vector& Vector::operator*= (const Vector& vec){
+  x*=vec.x;
+  y*=vec.y;
+  z*=vec.z;
+  return(*this);
+}
+
+Vector& Vector::operator* (const double val){
+  x*=val;
+  y*=val;
+  z*=val;
+  return(*this);
+}
+
+Vector& Vector::operator*= (const double val){
+  x*=val;
+  y*=val;
+  z*=val;
+  return(*this);
+}
+
+//Division
+Vector& Vector::operator/ (const Vector& vec){
+  x/=vec.x;
+  y/=vec.y;
+  z/=vec.z;
+  return(*this);
+}
+
+Vector& Vector::operator/= (const Vector& vec){
+  x/=vec.x;
+  y/=vec.y;
+  z/=vec.z;
+  return(*this);
+}
+
+Vector& Vector::operator/ (const double val){
+  x/=val;
+  y/=val;
+  z/=val;
+  return(*this);
+}
+
+Vector& Vector::operator/= (const double val){
+  x/=val;
+  y/=val;
+  z/=val;
+  return(*this);
+}
+
+
