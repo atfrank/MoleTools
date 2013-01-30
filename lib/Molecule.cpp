5f3e145b8c87f07b4436e1bd52aa4aa4d11fb693
Sean Law <seanlaw@umich.edu>
2013-01-30 15:11:56 -0500

    Fixed improper use of substring function

--- lib/Molecule.cpp
+++ lib/Molecule.cpp
@@ -16,25 +16,27 @@ int Molecule::readPDB (string *ifile){
     if (pdbFile.is_open()){
       while (pdbFile.good()){
         getline(pdbFile,line);
-        if (line.length() > 50 && line.compare(1,4,"ATOM") && line.compare(1,6,"HETATM")){
+        if (line.size() > 50 && (line.compare(0,4,"ATOM")==0 || line.compare(0,6,"HETATM")==0)){
+          //substr: first character is denoted by a value of 0 (not 1)
         /*
-          stringstream(line.substr(7,5)) >> atmnum;
-          atmname=line.substr(13,4);
-          alt=(line.substr(17,1))[0];
-          resname=line.substr(18,3);
-          chainid=(line.substr(22,1))[0];
-          stringstream(line.substr(23,4)) >> resid;
-          icode=(line.substr(27,1))[0];
+          stringstream(line.substr(6,5)) >> atmnum;
+          atmname=line.substr(12,4);
+          alt=(line.substr(16,1))[0];
+          resname=line.substr(17,3);
+          chainid=(line.substr(21,1))[0];
+          stringstream(line.substr(22,4)) >> resid;
+          icode=(line.substr(26,1))[0];
         */
-          stringstream(line.substr(31,8)) >> x;
-          stringstream(line.substr(39,8)) >> y;
-          stringstream(line.substr(47,8)) >> z;
+          stringstream(line.substr(30,8)) >> x;
+          stringstream(line.substr(38,8)) >> y;
+          stringstream(line.substr(46,8)) >> z;
         /*
           coor=Vector(x,y,z);
-          stringstream(line.substr(55,6)) >> occu;
-          stringstream(line.substr(61,6)) >> bfac;
-          segid=line.substr(73,4);
+          stringstream(line.substr(54,6)) >> occu;
+          stringstream(line.substr(60,6)) >> bfac;
+          segid=line.substr(72,4);
         */
+          cout << x << " " << y << " " << z << endl;
         }
       }
       pdbFile.close();
