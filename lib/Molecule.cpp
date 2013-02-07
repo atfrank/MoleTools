702a65e69eab4eb07907313a0e0cc6186d80d568
Sean Law <seanlaw@umich.edu>
2013-02-07 17:07:02 -0500

    Removed getLastRef functions

--- lib/Molecule.cpp
+++ lib/Molecule.cpp
@@ -54,37 +54,10 @@ unsigned int Molecule::getChnVecSize(){
   return chnVec.size();
 }
 
-Chain* Molecule::getLastChainRef(){
-  if (chnVec.size() >0){
-    return chnVec.at(chnVec.size()-1);
-  }
-  else{
-    return NULL;
-  }
-}
-
 unsigned int Molecule::getResVecSize(){
   return resVec.size();
 }
 
-Residue* Molecule::getLastResidueRef(){
-  if (resVec.size() > 0){
-    return resVec.at(resVec.size()-1);
-  }
-  else{
-    return NULL;
-  }
-}
-
 Residue* Molecule::getResidue(int element){
   return resVec.at(element);
-}
-
-Atom* Molecule::getLastAtomRef(){
-  if (atmVec.size() >0){
-    return atmVec.at(atmVec.size()-1);
-  }
-  else{
-    return NULL;
-  }
 }
--- lib/Molecule.hpp
+++ lib/Molecule.hpp
@@ -16,17 +16,14 @@ class Molecule {
     static Molecule* readPDB (std::string ifile, int model=0);
     int writePDB ();
     void addAtom(Atom* atmEntry);
-    Atom* getAtom(int element); //This should not be pointer!
+    Atom* getAtom(int element); 
     void addChain(Chain* chnEntry);
     void addResidue(Residue* resEntry);
     Chain* getChain(int element);
     unsigned int getChnVecSize();
-    Chain* getLastChainRef();
     unsigned int getResVecSize();
-    Residue* getLastResidueRef();
     unsigned int getAtmVecSize();
     Residue* getResidue(int element);
-    Atom* getLastAtomRef();
 };
 
 #endif
