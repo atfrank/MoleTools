4d70ac7c9aaabb8c9dc63916a86fd55934161c63
Sean Law <seanlaw@umich.edu>
2013-02-12 13:42:55 -0500

    Stable checkpoint before revamping selection

--- tests/test.sh
+++ tests/test.sh
@@ -3,5 +3,5 @@
 #../bin/manipPDB.exe -nsel A:ARG._B:140-145.CA ../tests/2DHB.pdb  > t.pdb
 #../bin/manipPDB.exe -nsel !A+B:1-137._!A+B:135+ARG. ../tests/2DHB.pdb  > t.pdb
 #../bin/manipPDB -nsel A:.CA ../tests/2DHB.pdb  > t.pdb
-../bin/manipPDB -nsel :1-135. ../tests/2DHB.pdb  > t.pdb
+../bin/manipPDB -nsel :^100-135. ../tests/2DHB.pdb  > t.pdb
 
