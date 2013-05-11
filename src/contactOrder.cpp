187e8e428f26fab2eeb20a99b6471f1ad7489e14
Sean M. Law <seanlaw@umich.edu>
2013-05-10 21:59:39 -0400

    Initialized files for contactOrder and kCluster

---
+++ src/contactOrder.cpp
@@ -0,0 +1,55 @@
+//Sean M. Law
+
+#include "Molecule.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+#define MAXINPFILES 4096
+#define MAXLINESIZE 4096
+
+void usage(){
+  exit(0);
+}
+
+int main (int argc, char **argv){
+
+
+  int i;
+  std::string pdb;
+  std::string currArg;
+  std::string sel;
+
+  pdb.clear();
+
+  for (i=1; i<argc; i++){
+    currArg=argv[i];
+    if (currArg == "-h" || currArg == "-help"){
+      usage();
+    }
+    else if (currArg == "-nsel"){
+      currArg=argv[++i];
+      sel=currArg;
+    }
+    else{
+      pdb=currArg;
+    }
+  }
+
+  if (pdb.length() == 0){
+    std::cerr << std::endl << "Error: Please provide an input file" << std::endl << std::endl;
+    usage();
+  }
+
+  Molecule *mol=Molecule::readPDB(pdb);
+
+  if (sel.length() >0){
+    mol->select(sel);
+  }
+
+  Molecule *cmol=mol->clone();
+  
+
+  return 0;
+}
