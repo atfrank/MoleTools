6bb855aff92948b75423ff5fbc69540884920ca6
Sean Law <seanlaw@umich.edu>
2013-05-30 11:19:56 -0400

    Initialized formatting

---
+++ lib/Format.hpp
@@ -0,0 +1,20 @@
+//Sean M. Law
+
+//Abstract base class (cannot create instance of it!)
+class Format {
+	private:
+
+	public:
+
+};
+
+//Derived classes
+class CHARMM: public Format {
+	public:
+		
+};
+
+class AMBER: public Format {
+	public:
+		
+};
