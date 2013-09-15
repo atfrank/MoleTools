38d9a4ab1d66a75a7326ea788cd074b9e1bf939e
Sean Law <seanlaw@umich.edu>
2013-09-14 22:34:22 -0400

    Minor change

--- lib/PDB.cpp
+++ lib/PDB.cpp
@@ -218,7 +218,7 @@ Molecule* PDB::readPDB(const std::string ifile, const int model, const std::stri
     mol->addChain(chnEntry);
   }
   
-  if (ifile.compare("-") != 0){
+  if (ifile.compare("-") != 0 && pdbFile.is_open()){
     pdbFile.close();
   }
 
