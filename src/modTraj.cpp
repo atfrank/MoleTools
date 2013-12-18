e4e747728c6cf68423a1fe45a487f3aaa7473912
Sean Law <seanlaw@umich.edu>
2013-12-18 15:59:36 -0500

    Added default -outsel to include all atoms ":."

--- src/modTraj.cpp
+++ src/modTraj.cpp
@@ -53,7 +53,7 @@ int main (int argc, char **argv){
 	double r1c1, r1c2, r1c3, r2c1, r2c2, r2c3, r3c1, r3c2, r3c3;
   bool rotate=false;
 	bool center=false;
-  std::string outsel="";
+  std::string outsel=":.";
   std::ifstream trjin;
   std::ofstream trjout;
 	Trajectory *ftrjin;
