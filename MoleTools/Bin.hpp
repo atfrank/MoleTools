4ed0200f1d3f3835fb1480b7a70e7aea59ac39ce
Sean Law <seanlaw@umich.edu>
2014-03-16 00:09:29 -0400

    Implemented Bin class and added it to Histogram

--- MoleTools/Bin.hpp
+++ MoleTools/Bin.hpp
@@ -3,13 +3,23 @@
 #ifndef BIN_H
 #define BIN_H
 
+#include <vector>
+
 class Bin {
   private:
-    struct bin {
-      unsigned int n;
-      double max;
-      double min;
-    };
+    unsigned int n;
+		unsigned int inx;
+		std::vector<double> label;
+	
+	public:
+		Bin();
+		void setN(const unsigned int &nin);
+		void setInx(const unsigned int &inxin);
+		void setLabel(const std::vector<double> &labelin);
+		unsigned int getN();
+		unsigned int getInx();
+		std::vector<double> getLabel();
+		std::vector<double>& getLabelVec();
 };
 
 #endif
