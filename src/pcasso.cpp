df40c4792a57f43b45c7faf7a8f1183af520f36e
Sean Law <seanlaw@umich.edu>
2014-04-10 10:28:53 -0400

    Implemented pcasso traj analysis

--- src/pcasso.cpp
+++ src/pcasso.cpp
@@ -2,9 +2,11 @@
 
 #include "Molecule.hpp"
 #include "Analyze.hpp"
+#include "Trajectory.hpp"
 
 #include <iostream>
 #include <ctime>
+#include <fstream>
 
 void usage(){
   std::cerr << "Usage:   pcasso [-options] <PDBfile>" << std::endl;
@@ -27,10 +29,22 @@ int main (int argc, char **argv){
   std::string dssp;
 	PcassoOutEnum out;
 	std::vector<std::string> trajs;
+	int start=0;
+	int stop=std::numeric_limits<int>::max();
+	int skip=0;
+	bool startFlag=false;
+	unsigned int itrj;
+	std::ifstream trjin;
+	Trajectory *ftrjin;
+	unsigned int nframe;
+	AnalyzePcasso *anin;
 
   dssp.clear();
 	out=PREDICT;
 	trajs.clear();
+	ftrjin=NULL;
+	nframe=0;
+	anin=NULL;
 
   for (i=1; i<argc; i++){
     currArg=argv[i];
@@ -51,6 +65,20 @@ int main (int argc, char **argv){
 			currArg=argv[++i];
 			trajs.push_back(currArg);
 		}
+		else if (currArg.compare("-skip") == 0){
+      currArg=argv[++i];
+      std::stringstream(currArg) >> skip;
+    }
+    else if (currArg.compare("-start") == 0){
+      currArg=argv[++i];
+      std::stringstream(currArg) >> start;
+      start--;
+      startFlag=true;
+    }
+    else if (currArg.compare("-stop") == 0){
+      currArg=argv[++i];
+      std::stringstream(currArg) >> stop;
+    }
 		else if (currArg.compare(0,1,"-") == 0){
       std::cerr << "Warning: Skipping unknown option \"" << currArg << "\"" << std::endl;
     }
@@ -64,12 +92,48 @@ int main (int argc, char **argv){
     usage();
   }
 
+	Molecule *mol=NULL;
+
 	if (trajs.size() > 0){
 		if (pdbs.size() > 1){
 			std::cerr << std::endl << "Warning: Only the first PDB structure is used for trajectory analysis" << std::endl << std::endl;
 		}
 		//Trajectory analysis
-
+		anin=new AnalyzePcasso;
+		anin->addSel(":.CA");
+		anin->setOutType(out);
+		mol=Molecule::readPDB(pdbs.at(0));
+		anin->preAnalysis(mol, "");
+		mol->selAll();
+		//Process trajectories
+		for (itrj=0; itrj< trajs.size(); itrj++){
+			trjin.open(trajs.at(itrj).c_str(), std::ios::binary);
+			if (trjin.is_open()){
+				ftrjin=new Trajectory;
+      	ftrjin->setMolecule(mol);
+				if (ftrjin->findFormat(trjin) == true){
+					ftrjin->readHeader(trjin);
+        	if (skip > 0 && startFlag == false){
+          	start=skip;
+        	}
+					//Loop through desired frames
+        	for (i=start; i< ftrjin->getNFrame() && i< stop; i=i+1+skip){
+          	ftrjin->readFrame(trjin, i);
+          	nframe++;
+						//Analyze PCASSO
+						anin->runAnalysis();					
+					}
+				}
+				else{
+					std::cerr << "Warning: Skipping unknown trajectory format \"";
+        	std::cerr << trajs.at(itrj) << "\"" << std::endl;
+				}
+				if (ftrjin != NULL){
+					delete ftrjin;
+				}
+			}
+			trjin.close();
+		}
 	}
   else if (dssp.length() > 0){
     if (pdbs.size() > 1){
@@ -77,19 +141,19 @@ int main (int argc, char **argv){
       std::cerr << "Warning: Only the first PDB structure was processed" << std::endl;
     }
     //Only process first structure!
-    Molecule *mol=Molecule::readPDB(pdbs.at(0));
+    mol=Molecule::readPDB(pdbs.at(0));
     std::cerr << "Processing file \"" << pdbs.at(0) << "..." << std::endl;
-	mol->pcasso(dssp, out); //Makes temporary clone with C-alpha only, and analyzes it
+		mol->pcasso(dssp, out); //Makes temporary clone with C-alpha only, and analyzes it
     delete mol;
   }
   else{
 		//Placed here for efficiency; construct trees once instead of calling mol->pcasso()
-		AnalyzePcasso* anin=new AnalyzePcasso;
+		anin=new AnalyzePcasso;
 		anin->addSel(":.CA");
 		anin->setOutType(out);
 		
 	  for (j=0; j< pdbs.size(); j++){
-  	  Molecule *mol=Molecule::readPDB(pdbs.at(j));
+  	  mol=Molecule::readPDB(pdbs.at(j));
 		  std::cerr << "Processing file \"" << pdbs.at(j) << "..." << std::endl;
 		
 			/*
@@ -112,6 +176,8 @@ int main (int argc, char **argv){
 
 		  delete mol;
     }
+	}
+	if (anin != NULL){
 		delete anin;
 	}
 
