24c518a839e1056bcfa7c752ac0cea6fc96dbb77
Sean Law <seanlaw@umich.edu>
2014-04-11 09:22:28 -0400

    Changed unsigned long long int to streampos

--- lib/Trajectory.cpp
+++ lib/Trajectory.cpp
@@ -588,7 +588,7 @@ unsigned int Trajectory::getLastFrame(){
   return lastFrame;
 }
 
-unsigned long long int Trajectory::getLastPos(){
+std::streampos Trajectory::getLastPos(){
 	return lastPos;
 }
 
@@ -712,7 +712,7 @@ void Trajectory::setLastFrame(const unsigned int &frame){
   lastFrame=frame;
 }
 
-void Trajectory::setLastPos(const unsigned long long int &pos){
+void Trajectory::setLastPos(const std::streampos &pos){
 	lastPos=pos;
 }
 
