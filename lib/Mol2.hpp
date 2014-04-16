d220a93851fc7b6b0c46b75ce37af5a6c569bbc3
Sean Law <seanlaw@umich.edu>
2014-04-16 08:27:29 -0400

    Added GPL license

--- lib/Mol2.hpp
+++ lib/Mol2.hpp
@@ -1,4 +1,23 @@
 //Sean M. Law
+//Aaron T. Frank
+    
+/*
+This file is part of MoleTools.
+
+MoleTools is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MoleTools is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MoleTools.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef MOL2_H
 #define MOL2_H
 
