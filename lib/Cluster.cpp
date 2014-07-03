fd12ab7a6a6602782b64a5f8bac75dda5f71bc49
Sean Law <seanmylaw@gmail.com>
2014-07-03 16:21:33 -0400

    Further progress on DP Cluster

---
+++ lib/Cluster.cpp
@@ -0,0 +1,92 @@
+//Sean M. Law
+//Aaron T. Frank
+
+/*
+This file is part of MoleTools.
+
+MoleTools is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MoleTools is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MoleTools.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Cluster.hpp"
+
+Cluster::Cluster(unsigned int Nin){
+  this->setN(Nin);
+  cluster.resize(Nin);
+  dmatrix.resize(Nin);
+  for (unsigned int i=0; i< Nin; i++){
+    dmatrix.at(i).resize(Nin);
+  }
+}
+
+void Cluster::setN(unsigned int Nin){
+  N=Nin;
+}
+
+unsigned int Cluster::getN(){
+  return N;
+}
+
+void Cluster::preCluster(){
+
+}
+
+void Cluster::runCluster(){
+  //Do Nothing
+}
+
+void DPCluster::preCluster(){
+  
+}
+
+void DPCluster::runCluster(){
+
+}
+
+void DPCluster::setDc(const double& dcin){
+  dc=dcin;
+}
+    
+double DPCluster::getDc(){
+  return dc;
+}
+
+void DPCluster::setDmax(const double& dmaxin){
+  dmax=dmaxin;
+}
+
+double DPCluster::getDmax(){
+  return dmax;
+}
+
+void DPCluster::setMaxDelta(const double& maxdeltain){
+  maxDelta=maxdeltain;
+}
+
+double DPCluster::getMaxDelta(){
+  return maxDelta;
+}
+
+void DPCluster::setRho(){
+  this->rho.resize(this->getN());
+
+}
+
+void DPCluster::setDelta(){
+  this->delta.resize(this->getN());
+  this->setMaxDelta(0.0);
+}
+
+void KCluster::runCluster(){
+
+}
---
+++ lib/Cluster.hpp
@@ -0,0 +1,71 @@
+//Sean M. Law
+//Aaron T. Frank
+
+/*
+This file is part of MoleTools.
+
+MoleTools is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MoleTools is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MoleTools.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CLUSTER_H
+#define CLUSTER_H
+
+#include <vector>
+
+class Cluster {
+  private:
+    unsigned int N;
+    std::vector<std::vector <double> > dmatrix; //NxN Distance matrix
+    std::vector<unsigned int> cluster; //Store cluster numbers for each N datapoints
+
+  public:
+    Cluster(unsigned int Nin);
+    void setN(unsigned int Nin);
+    unsigned int getN();
+
+    //Virtual Functions
+    virtual void preCluster();
+    virtual void runCluster()=0;
+};
+
+class DPCluster: public Cluster {
+  private:
+    double dc;
+    double dmax;
+    double maxDelta;
+    std::vector<unsigned int> rho;
+    std::vector<double> delta;
+
+  public:
+    void preCluster();
+    void runCluster();
+    void setDc(const double& dcin);
+    double getDc();
+    void setDmax(const double& dmaxin);
+    double getDmax();
+    void setMaxDelta(const double& maxdeltain);
+    double getMaxDelta();
+    void setRho();
+    void setDelta();
+
+};
+
+class KCluster: public Cluster {
+  private: 
+
+  public:
+    void runCluster();
+};
+
+#endif
